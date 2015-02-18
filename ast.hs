f36d69ba199d34dd730434bb2fe900369450d08e
patrick <snixromero@gmail.com>
2015-02-17 17:32:45 -0800

    added some constant folding-not yet done

--- ast.hs
+++ ast.hs
@@ -1,4 +1,4 @@
-data Func = Unop UOP Func | Binop BOP Func Func | Constant Double | Variable VAR
+data Func = Unop UOP Func | Binop BOP Func Func | Constant Double | Variable VAR | Real RE
 	deriving Show
 
 data UOP = NEG | SIN | COS | TAN | EXP | LN | ASIN | ACOS | ATAN | SQRT | INV
@@ -8,6 +8,8 @@ data BOP = ADD | SUB | MUL | DIV | POW | LOG
 	deriving (Show, Eq)
 
 type VAR = String
+
+data RE = PI | E
 
 differentiate :: Func -> VAR -> Func
 --differentiate a function with respect to a specific variable
@@ -41,6 +43,88 @@ differentiate (Binop b f g) x
 	| b == POW = Binop MUL (Binop POW f $ Binop SUB g $ Constant 1.0) $ Binop ADD (Binop MUL g $ differentiate f x)  $ Binop MUL f (Binop MUL (Unop LN f) $ differentiate g x)
 	| b == LOG = Binop DIV (Binop SUB (Binop DIV (Binop MUL (Unop LN f) $ differentiate g x) g) $ Binop DIV (Binop MUL (Unop LN g) $ differentiate f x) f) $ Binop POW (Unop LN f) $ Constant 2.0
 	| otherwise = Constant 0.0
+
+diff_n :: Func -> Int -> VAR -> Func
+
+diff_n f 0 _ = f
+
+diff_n f n x = differentiate (diff_n f (n-1) x) x
+
+
+eval :: Func -> Func
+
+eval Unop u (Constant c)
+	| u == NEG = Constant (-c)
+	| u == SIN = Constant $ sin c
+	| u == COS = Constant $ cos c
+	| u == TAN = Constant $ tan c
+	| u == EXP = Constant $ exp c
+	| u == LN  = Constant $ log c
+	| u == SQRT= Constant $ sqrt c
+	| u == INV = Constant $ 1.0 / c
+	| u == ASIN= Constant $ asin c
+	| u == ACOS= Constant $ acos c
+	| u == ATAN= Constant $ atan c
+	| otherwise = Constant 0.0
+
+eval Binop b (Constant c1) (Constant c2)
+	| b == ADD = Constant $ c1 + c2
+	| b == SUB = Constant $ c1 - c2
+	| b == MUL = Constant $ c1 * c2
+	| b == DIV = Constant $ c1 / c2
+	| b == POW = Constant $ c1 ^ c2
+	| b == LOG = Constant $ logBase c1 c2
+
+eval Unop u f = let v = eval f
+		in case v of Constant c -> eval $ Unop u $ Constant c
+			     otherwise -> Unop u v
+
+eval Binop b (Constant c1) g = let v = eval g
+			      in case v of Constant c2 -> eval $ Binop b (Constant c1) (Constant c2)
+					   otherwise -> Binop b (Constant c1) v
+					   
+	
+eval Binop b f (Constant c2) = let v = eval f
+			      in case v of Constant c1 -> eval $ Binop b (Constant c1) (Constant c2)
+					   otherwise -> Binop b v (Constant c1)
+
+eval Binop b f g = let v1 = eval f
+		   in case v1 of (Constant c1) -> eval $ Binop b (Constant c1) g
+				 otherwise -> let v2 = eval g
+					      in case v2 of (Constant c2) -> eval $ Binop b v1 (Constant c2)
+							    otherwise -> Binop b v1 v2
+eval Variable v = Variable v
+
+
+--identities
+
+
+eval Binop ADD (Constant 0.0) g = g
+eval Binop ADD f (Constant 0.0) = f
+
+eval Binop SUB (Constant 0.0) g = eval $ Unop NEG g
+eval Binop SUB f (Constant 0.0) = f
+
+eval Binop MUL (Constant 1.0) g = g
+eval Binop MUL f (Constant 1.0) = f
+eval Binop MUL (Constant 0.0) g = Constant 0.0
+eval Binop MUL f (Constant 0.0) = Constant 0.0
+
+eval Binop DIV f (Constant 1.0) = f
+
+eval Binop POW f (Constant 1.0) = f
+eval Binop POW f (Constant 0.0) = Constant 0.0
+
+eval Binop LOG f (Constant 1.0) = Constant 0.0
+
+eval 
+
+
+
+
+
+
+
 	
 main :: IO()
 
